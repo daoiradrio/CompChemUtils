92e5f13160466a16758f3e2cb3405b8a266130ac
Dario Baum <dario-cbr@hotmail.de>
2024-02-08 16:29:01 +0100

    Added function for plotting matrices in a nice way to Visual module

--- visual.py
+++ visual.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -16,3 +17,17 @@ def printvec(vec: np.array) -> None:
     for val in vec:
         print(f"{val:15.8f}")
     print()
+
+
+
+def plotmat(mat: np.array) -> None:
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(mat, vmin=-1, vmax=1, cmap="PiYG")
+    for i in range(mat.shape[1]):
+        for j in range(mat.shape[1]):
+            ax.text(i, j, "%.2f" % mat[j,i], ha="center", va="center", fontsize=7)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.hlines(np.arange(1, mat.shape[0])-0.5, -0.5, mat.shape[0]-0.5, color="black", linestyle="--", linewidth=1)
+    ax.vlines(np.arange(1, mat.shape[0])-0.5, -0.5, mat.shape[0]-0.5, color="black", linestyle="--", linewidth=1)
+    plt.show()
