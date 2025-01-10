71d1166745cc71e0c17347adc4ff93b79309bfc4
Dario Baum <dario-cbr@hotmail.de>
2025-01-10 11:23:58 +0100

    Add option to read elements as atomic numbers from XYZ file

--- files.py
+++ files.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
 
+from CompChemUtils.chemdata import nuclear_charges
 
 
-def read_xyz_file(filepath: str) -> (int, list, np.array):
+
+def read_xyz_file(filepath: str, element_symbols=True):
     if not os.path.isfile(filepath):
         print()
         print("*********** ERROR ***********")
@@ -13,18 +15,20 @@ def read_xyz_file(filepath: str) -> (int, list, np.array):
         return None
     elems = list(np.loadtxt(filepath, skiprows=2, usecols=0, dtype=str))
     coords = np.loadtxt(filepath, skiprows=2, usecols=(1,2,3))
-    natoms = len(elems)
-    return natoms, elems, coords
+    num_atoms = len(elems)
+    if not element_symbols:
+          elems = np.array([nuclear_charges[elem] for elem in elems])
+    return num_atoms, elems, coords
 
 
-def write_xyz_file(filepath: str, elems: list, coords: np.array) -> None:
+def write_xyz_file(filepath: str, elems: list, coords: np.array):
 	with open(filepath, "w") as xyzfile:
 		print(coords.shape[0], file=xyzfile, end="\n\n")
 		for elem, (x,y,z) in zip(elems, coords):
 			print(f"{elem}\t{x:18.10f}\t{y:18.10f}\t{z:18.10f}", file=xyzfile)
 
 
-def write_file(filepath: str, contentstr: str) -> None:
+def write_file(filepath: str, contentstr: str):
     with open(filepath, "w") as outfile:
         print(contentstr, file=outfile)
     
